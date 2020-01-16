19d73421748c36a78f1574a0e4b18682204316f4
adiloff <samper818@gmail.com>
2020-01-16 15:11:28 +0500

    finish

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ var numPrompt = +prompt('Введите ваш возраст');
 var numPrompt2 = +prompt('Решите пример 31-27');
 var numPrompt3 = +prompt('Решите пример 12+17');
 var numPrompt4 = +prompt('Решите пример 18*2');
-var numPrompt4 = +prompt('Решите пример 8/4');
-var numPrompt5 = +prompt('Решите пример 10 % 3');
+var numPrompt5 = +prompt('Решите пример 8/4');
+var numPrompt6 = +prompt('Решите пример 10 % 3');
+alert('Откройте консоль для просмотра')
 console.log('Меня зовут ' + strName);
 console.log('Мне ' + numPrompt);
 console.log('Пример 1: 31 - 27 = 4 Ваш ответ ' + numPrompt2);
 console.log('Пример 2: 12 + 17 = 29 Ваш ответ ' + numPrompt3);
-console.log('Пример 3: 12 + 17 = 29 Ваш ответ ' + numPrompt3);
+console.log('Пример 3: 18 * 2 = 36 Ваш ответ ' + numPrompt4);
+console.log('Пример 3: 8 / 4 = 2 Ваш ответ ' + numPrompt5);
+console.log('Пример 4: 10 % 3 = 1 Ваш ответ ' + numPrompt6);
